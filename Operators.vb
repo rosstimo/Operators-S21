344207452e2f5b522bb214f4578234662307ccba
Tim Rossiter <48777447+rosstimo@users.noreply.github.com>
2021-01-13 08:33:17 -0700

    Add project files.

---
+++ Operators.vb
@@ -0,0 +1,17 @@
+﻿Module Operators
+
+    Sub Main()
+
+        Console.WriteLine(2 + 2)
+        Console.WriteLine(8 - 2)
+        Console.WriteLine(5 * 5)
+
+        Console.WriteLine(1 / 2)        'Regular Div
+        Console.WriteLine(1 \ 2)        'Integer
+        Console.WriteLine(9.8 Mod 3)    'division returns only the remainder
+
+        Console.ReadLine()
+
+    End Sub
+
+End Module
