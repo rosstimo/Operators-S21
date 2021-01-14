40178332976f5e3ffde5acd6678858d0146cccc0
Tim Rossiter <48777447+rosstimo@users.noreply.github.com>
2021-01-14 08:53:55 -0700

    text compare

--- Operators.vb
+++ Operators.vb
@@ -1,20 +1,39 @@
-﻿Module Operators
+﻿'Tim Rossiter
+
+'https://github.com/rosstimo/Operators-S21.git
+
+
+
+Option Compare Text
+
+Module Operators
 
     Sub Main()
 
-        Console.WriteLine(2 + 2)
-        Console.WriteLine(8 - 2)
-        Console.WriteLine(5 * 5)
+        'Console.WriteLine(2 + 2)
+        'Console.WriteLine(8 - 2)
+        'Console.WriteLine(5 * 5)
 
-        Console.WriteLine(1 / 2)        'Regular Div
-        Console.WriteLine(1 \ 2)        'Integer
-        Console.WriteLine(9.8 Mod 3)    'division returns only the remainder
+        'Console.WriteLine(1 / 2)        'Regular Div
+        'Console.WriteLine(1 \ 2)        'Integer
+        'Console.WriteLine(9.8 Mod 3)    'division returns only the remainder
 
-        Console.WriteLine(5 ^ 5)
+        'Console.WriteLine(5 ^ 5)
 
-        Console.WriteLine("hello" & "World")
+        'Console.WriteLine("hello" & " World")
+        'Console.WriteLine("hello" & "5")
 
 
+        'compare stuff
+
+        'Console.WriteLine(3 > 4)
+        'Console.WriteLine(3 < 4)
+        'Console.WriteLine(3 = 4)
+        'Console.WriteLine(3 <> 4)
+
+        'Console.WriteLine("a" > "A")
+        'Console.WriteLine("AB" < "ABA")
+        Console.WriteLine("abc" = "ABC")
         Console.ReadLine()
 
     End Sub
